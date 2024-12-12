0b59f8a9a304e81db40e5ea4791593f7cb0bc5b0
Phatcharphat <n.phatcharphat@gmail.com>
2024-12-12 15:53:06 +0700

    first comit

--- cypress.config.js
+++ cypress.config.js
@@ -1,36 +1,11 @@
 const { defineConfig } = require("cypress");
-const fs = require('fs');
 
-module.exports = {
+module.exports = defineConfig({
+  projectId: "fdxewj",
+
   e2e: {
     setupNodeEvents(on, config) {
-      on('task', {
-        commentCustomer({ filePath, customerIndex }) {
-          try {
-            let fileContent = fs.readFileSync(filePath, 'utf-8');
-            let lines = fileContent.split('\n');
-
-            // ค้นหาและ comment บรรทัดที่มีข้อมูลลูกค้าตาม index
-            let currentIndex = 0;
-            lines = lines.map((line) => {
-              if (!line.startsWith('//') && currentIndex === customerIndex) {
-                currentIndex++;
-                return `// ${line}`;
-              } else if (!line.startsWith('//')) {
-                currentIndex++;
-              }
-              return line;
-            });
-
-            // เขียนข้อมูลกลับไปที่ไฟล์
-            fs.writeFileSync(filePath, lines.join('\n'), 'utf-8');
-            return null;
-          } catch (error) {
-            console.error(error);
-            return null;
-          }
-        }
-      });
-    }
-  }
-};
+      // implement node event listeners here
+    },
+  },
+});
